df14534c61268a667cc03d769d9f78bb33c6aaf5
jimmy_zeng <jimmy_zeng@192.168.1.103>
2016-03-31 22:19:41 +0800

    最新版

--- JAVSpider.py
+++ JAVSpider.py
@@ -1,27 +1,63 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import socket
 import urllib2
 import bt2mag
 import time
 
 numberList = []
-driver = webdriver.PhantomJS()
-for index in range(1,7):
-    strUrl = "http://www.javmoo.xyz/cn/star/6qd/currentPage/"+str(index)
+errorNumberList = []
+magnetList = []
+driver = webdriver.Firefox()
+socket.setdefaulttimeout(200)
+time.sleep(30)
+fo = open('mingrihuaqiluo.txt','wb+')
+for index in range(1,11):
+    strCurrentPage = "/currentPage/" + str(index)
+    strUrl = "https://www.avmoo.com/cn/star/1ny" + strCurrentPage
     print strUrl
-    response = urllib2.urlopen(strUrl)
-    html = response.read()
-    soup = BeautifulSoup(html,"html.parser")
+    # response = urllib2.urlopen(strUrl)
+    # html = response.read()
+    try:
+        driver.get(strUrl)
+    except socket.timeout:
+        print "time out"
+    try:
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+    except Exception as err:
+        print "error ------------------------------------------- " + strUrl
     result = soup.find_all(name="date")
-    time.sleep(3)
+    time.sleep(2)
     for indexForDate in range(0,len(result)):
         if indexForDate%2 == 0:
             strResult = result[indexForDate]
-            iStart = str(strResult).find('<date>',0)+6
+            iStart = str(strResult).find('<date>',0) + 6
             iEnd = str(strResult).find('</date>',0)
             strResult = str(strResult)[iStart:iEnd]
             numberList.append(strResult)
     for number in numberList:
-        if(bt2mag.FindMagnet(number,driver) == False):
-            print "didn't find                       " + str(number)
+        magnet = bt2mag.FindMagnet(number,driver,1)
+        if(len(magnet) <= 0):
+            errorNumberList.append(number)
+        else:
+            if(len(magnet) > 5):
+                magnetList.append(magnet)
+    numberList = []
+reStartList = errorNumberList
+errorNumberList = []
+for error in reStartList:
+    magnet = bt2mag.FindMagnet(error,driver,2)
+    if(len(magnet) <= 0):
+        errorNumberList.append(error)
+    else:
+        if(len(magnet) > 5):
+            magnetList.append(magnet)
+
+for magnetOne in magnetList:
+    fo.write(magnetOne + "\n")
+
+for error in errorNumberList:
+    print error
+
+fo.close()
 driver.close()
--- bt2mag.py
+++ bt2mag.py
@@ -1,47 +1,92 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
+import BeautifulSoup
 import time
+import socket
+import sys
+import re
 
-def getMagnet(rowArray, list, driver):
+reload(sys)
+sys.setdefaultencoding('utf-8')
+socket.setdefaulttimeout(200)
+def getMagnet(rowArray, strNumber, list, driver):
+    # bBool = False
     for passOne in list:
-        bPass = False
+        bPass = ""
         for row in rowArray:
-            if (str(row).find(str(passOne)) >= 0):
-                bPass = True
+            arrSize = row.find_all(name = 'div',attrs = {'class':'col-xs-12 size-date visible-xs-block' })
+            size = arrSize[0].string
+            iEnd = size.find('GB')
+            fSize = float(size[5:iEnd])
+            if ((fSize > 3.0) and (fSize < 10.0)):
+                title =  row['title']
+                strinfo = re.compile('-')
+                _strNumber = strinfo.sub('_',strNumber)
+                if (title.find('DVD') < 0 and title.find('@') < 0 and title.find('iso') < 0 and (title.find(strNumber) > 0 or title.find(_strNumber))):
+                    # bBool = True
+                    bPass = "pass"
+                    break
+            if (str(row['title']).find(str(passOne)) > 0):
+                bPass = "pass"
                 break
-        if (bPass):
-            iStart = str(row).find('href=', 0) + 6
-            iEnd = str(row).find('title', 0) - 2
-            strResult = str(row)[iStart:iEnd]
-            time.sleep(3)
-            driver.get(strResult)
-            soup = BeautifulSoup(driver.page_source, "html.parser")
-            magnet = soup.textarea
+        # if (bBool):
+        #     break
+        if (len(bPass) > 1):
+            strResult = row.get('href')
+            time.sleep(2)
+            try:
+                driver.get(strResult)
+            except socket.timeout:
+                print "time out"
+            try:
+                soupForMagnet = BeautifulSoup(driver.page_source, "html.parser")
+            except Exception as err:
+                print "error ++++++++++++++++++++++++++++++ " + strResult
+                return ""
+            magnet = soupForMagnet.textarea
             if (str(magnet).find('None') < 0):
-                print magnet.string
+                return magnet.string
             else:
                 print "error~!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
                 print rowArray
                 print row
                 print strResult
                 print magnet
-                bPass = False
+                bPass = ""
             break
 
     return bPass
 
-def FindMagnet(strNumber, driver):
-    strUrl = "http://www.bt2mag.com/search/" + strNumber
-    listHightQuality = ['.1080p', '.720p', '[1080p]', '[720p]', '-1080p','-720p','[FHD]', '-FHD', '[Thz.la]']
-    listOrdinaryQuality = [strNumber + '.mp4', str(strNumber).lower() + '.mp4',strNumber + '.avi',str(strNumber).lower() + '.avi','@SIS001@',strNumber,str(strNumber).lower()]
-    time.sleep(3)
-    driver.get(strUrl)
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-    rowArray = soup.find_all(name='a',attrs={'class':''})
-    bPass = False
+def FindMagnet(strNumber, driver,iQuality):
+    strUrl = "https://btio.pw/search/" + strNumber
+    listHightQuality = ['.1080p', '.720p', '[1080p]', '[720p]', '-1080p','-720p','[FHD]', '-FHD', '[Thz.la]','[HD]','(720)','(1080)',strNumber + '-HD','-FULLHD' ,'_FHD','_HD']
+    listOrdinaryQuality = [strNumber + '.mp4', str(strNumber).lower() + '.mp4',strNumber + '.avi',str(strNumber).lower() + '.avi','@SIS001@']
+    time.sleep(2)
+    try:
+        driver.get(strUrl)
+    except socket.timeout:
+        print "time out"
 
-    bPass = getMagnet(rowArray, listHightQuality, driver)
-    if (bPass == False):
-        bPass = getMagnet(rowArray, listOrdinaryQuality, driver)
+    try:
+        soupForSearch = BeautifulSoup(driver.page_source, "html.parser")
+    except Exception as err:
+        print "error ==================== " + strUrl
+        return ""
 
+    aArray = []
+    dataListArray = soupForSearch.find_all(name='div',attrs={'class':'data-list'})
+    if(len(dataListArray) <= 0):
+        return "null"
+    for dataList in dataListArray:
+         rowArray = dataList.find_all(name = 'div',attrs = {'class':'row'})
+         for row in rowArray:
+             sizeArray = row.find_all(name = 'div',attrs = {'class':'col-sm-2 col-lg-1 hidden-xs text-right size'})
+             for size in sizeArray:
+                 strSize = size.string
+                 if (str(strSize).find('GB') > 0):
+                     aArr = row.find_all(name = 'a')
+                     if(aArr.count > 0):
+                         aArray.extend(aArr)
+    if(int(iQuality) == 1):
+        bPass = getMagnet(aArray, strNumber , listHightQuality, driver)
+    elif(int(iQuality) == 2):
+        bPass = getMagnet(aArray, strNumber , listOrdinaryQuality, driver)
     return bPass
